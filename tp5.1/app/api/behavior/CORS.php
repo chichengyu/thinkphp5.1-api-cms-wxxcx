58e4dd05405a197c44f49228fcd92da2657cc6f8
chichengyu <31581269+chichengyu@users.noreply.github.com>
2018-12-29 16:02:22 +0800

    Update CORS.php

--- tp5.1/app/api/behavior/CORS.php
+++ tp5.1/app/api/behavior/CORS.php
@@ -9,12 +9,12 @@ class CORS
 		// 允许所有的域访问
 		header('Access-Control-Allow-Origin: http://123.com');
 		// 允许访问的时候header允许携带的那些键值对
-        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, token");
-        // 允许访问的方式
-        header('Access-Control-Allow-Methods: POST,GET');
-        // 一般不允许复杂请求(即options请求)
-        if (request()->isOptions()) {
-        	exit();
-        }
+        	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, token");
+        	// 允许访问的方式
+        	header('Access-Control-Allow-Methods: POST,GET');
+        	// 一般不允许复杂请求(即options请求)
+        	if (request()->isOptions()) {
+        		exit();
+        	}
 	}
 }
